458b5fd3775e77aeee86dd42b56cd4ed68293c6c
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-24 22:57:20 -0300

    feat: add entities

---
+++ FunNTalk.Domain/Entities/ChatRoomEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace FunNTalk.Domain.Entities;
+
+public class ChatRoomEntity(string name)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; init; } = name;
+    public List<UserEntity> Participants { get; set; } = [];
+}
---
+++ FunNTalk.Domain/Entities/UserEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace FunNTalk.Domain.Entities;
+
+public class UserEntity(string username, string connectionId)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Username { get; init; } = username;
+    public string ConnectionId { get; init; } = connectionId;
+}
