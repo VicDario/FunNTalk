f6e20507f3edf8ece105de81d570049d103221a9
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-26 15:09:35 -0300

    feat: add messagedto

---
+++ FunNTalk.Domain/DTOs/MessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FunNTalk.Domain.DTOs;
+
+public class MessageDto(DateTime timestamp, string user, string message)
+{
+    public DateTime Timestamp { get; init; } = timestamp;
+    public string User { get; init; } = user;
+    public string Message { get; init; } = message;
+}
