d8539fe48f32b5ba3ab82b680729b97f886ba9bd
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-28 20:16:32 -0300

    feat: add comunication controller

---
+++ FunNTalk.API/Controllers/CommunicationController.cs
@@ -0,0 +1,22 @@
+﻿using FunNTalk.Domain.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunNTalk.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class CommunicationController(IGetUsersFromRoomUseCase getUsersFromRoomUseCase) : ControllerBase
+{
+    private readonly IGetUsersFromRoomUseCase _getUsersFromRoomUseCase = getUsersFromRoomUseCase;
+
+    [HttpGet]
+    [Route("room/{roomName}/participants")]
+    public IActionResult GetUsersFromRoom([FromRoute] string roomName)
+    {
+        var users = _getUsersFromRoomUseCase.Execute(roomName);
+
+        if (users == null) return NotFound(new { Message = $"No users found for room {roomName}." });
+
+        return Ok(users);
+    }
+}
