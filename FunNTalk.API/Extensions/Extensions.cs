d55da39b3b3b02886622b44662495d3e05d75fb0
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-28 20:16:54 -0300

    feat(routing)!: transform routes to lowercase

--- FunNTalk.API/Extensions/Extensions.cs
+++ FunNTalk.API/Extensions/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FunNTalk.API.Extensions;
 
@@ -6,6 +7,13 @@ public static class Extensions
 {
     public static void ApiConfigure(this IServiceCollection services)
     {
+        services.Configure<RouteOptions>(
+            options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            }
+        );
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
