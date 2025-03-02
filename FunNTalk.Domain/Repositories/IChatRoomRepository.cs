ba7bbe2b18e5573c3ef5b5c73a8bf21e5cf3ad6b
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-02 15:38:14 -0300

    feat: add get user method

--- FunNTalk.Domain/Repositories/IChatRoomRepository.cs
+++ FunNTalk.Domain/Repositories/IChatRoomRepository.cs
@@ -8,4 +8,5 @@ public interface IChatRoomRepository
     void AddUserToRoom(string roomName, UserEntity user);
     UserEntity? RemoveUserFromRoom(string roomName, string connectionId);
     UserEntity? GetUserFromRoom(string roomName, string connectionId);
+    UserEntity? GetUser(string connectionId);
 }
