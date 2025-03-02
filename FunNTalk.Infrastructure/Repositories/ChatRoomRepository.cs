06c06c1181a53a1cc299414b6638a697900f0c9c
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-02 15:20:25 -0300

    feat: use concurrent dictionaries and add user dictionary to help with remove

--- FunNTalk.Infrastructure/Repositories/ChatRoomRepository.cs
+++ FunNTalk.Infrastructure/Repositories/ChatRoomRepository.cs
@@ -1,22 +1,21 @@
 ﻿using FunNTalk.Domain.Entities;
 using FunNTalk.Domain.Repositories;
+using System.Collections.Concurrent;
 
 namespace FunNTalk.Infrastructure.Repositories;
 
 public sealed class ChatRoomRepository : IChatRoomRepository
 {
-    private readonly Dictionary<string, ChatRoomEntity> _rooms = [];
+    private readonly ConcurrentDictionary<string, ChatRoomEntity> _rooms = [];
+    private readonly ConcurrentDictionary<string, UserEntity> _users = [];
+
     public void AddUserToRoom(string roomName, UserEntity user)
     {
+        _users.TryAdd(user.ConnectionId, user);
         if (_rooms.TryGetValue(roomName, out var room))
             room.Participants.Add(user);
         else
             CreateRoom(roomName, user);
-    }
-
-    private void CreateRoom(string roomName, UserEntity user)
-    {
-        _rooms.Add(roomName, new ChatRoomEntity(roomName) { Participants = [user] });
     }
 
     public ChatRoomEntity? GetRoom(string roomName)
@@ -31,6 +30,9 @@ public sealed class ChatRoomRepository : IChatRoomRepository
         var user = room?.Participants.Find(participant => participant.ConnectionId == connectionId);
         if (user is null) return null;
         room?.Participants.Remove(user);
+        if (room?.Participants.Count == 0)
+            RemoveRoom(roomName);
+        _users.TryRemove(connectionId, out _);
         return user;
     }
 
@@ -38,5 +40,21 @@ public sealed class ChatRoomRepository : IChatRoomRepository
     {
         var room = GetRoom(roomName);
         return room?.Participants.FirstOrDefault(participant => participant.ConnectionId == connectionId);
+    }
+
+    public UserEntity? GetUser(string connectionId)
+    {
+        _users.TryGetValue(connectionId, out var user);
+        return user;
+    }
+
+    private void CreateRoom(string roomName, UserEntity user)
+    {
+        _rooms.TryAdd(roomName, new ChatRoomEntity(roomName) { Participants = [user] });
+    }
+
+    private void RemoveRoom(string roomName)
+    {
+        _rooms.TryRemove(roomName, out _);
     }
 }
