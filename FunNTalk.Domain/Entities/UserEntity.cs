e983ba3258413b911724b4eacd4159cfa4b61c0a
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-02 15:18:46 -0300

    feat: add room field

--- FunNTalk.Domain/Entities/UserEntity.cs
+++ FunNTalk.Domain/Entities/UserEntity.cs
@@ -1,8 +1,9 @@
 ﻿namespace FunNTalk.Domain.Entities;
 
-public class UserEntity(string username, string connectionId)
+public class UserEntity(string username, string connectionId, string room)
 {
     public Guid Id { get; } = Guid.NewGuid();
     public string Username { get; init; } = username;
     public string ConnectionId { get; init; } = connectionId;
+    public string Room { get; init; } = room;
 }
